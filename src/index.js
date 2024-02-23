957f8b7f9481ee0b58a81ee0846a9b4c42c3f932
Jakub Trafimovich <5243212@gmail.com>
2024-02-23 11:07:02 +0100

    refactor: moved all calculations to helpers

--- src/index.js
+++ src/index.js
@@ -1,47 +1,17 @@
 const { config } = require('./config');
 const {
-  calculateCashInCommission,
-  calculateCashOutNaturalCommission,
-  calculateCashOutJuridicalCommission,
+  calculateResult,
   roundToInteger,
 } = require('./helpers/commissionUtils');
 const { readInputFromFile } = require('./helpers/fileUtils');
 
-const calculateCommission = (operation, config) => {
-  const { type, operation: { amount }, user_type } = operation;
-  const { cashIn, cashOutNatural, cashOutJuridical } = config;
-
-  switch (type) {
-    case 'cash_in':
-      return calculateCashInCommission(amount, cashIn.max.amount);
-    case 'cash_out':
-      return user_type === 'natural'
-        ? calculateCashOutNaturalCommission(
-          amount,
-          cashOutNatural.week_limit.amount,
-          cashOutNatural.weekOperations
-        )
-        : calculateCashOutJuridicalCommission(
-          amount,
-          cashOutJuridical.min.amount
-        );
-    default:
-      console.error('Unknown operation type:', type);
-      return null;
-  };
-};
-
-const calculateResult = (inputData, config) => {
-  return inputData.map(operation => {
-    const commission = calculateCommission(operation, config);
-    return roundToInteger(commission).toFixed(2);
-  });
-};
-
 const feesCalculating = (inputFilePath) => {
   const inputData = readInputFromFile(inputFilePath);
   const result = calculateResult(inputData, config);
-  result.forEach(commission => console.log(commission));
+
+  result.forEach((commission) =>
+    console.log(roundToInteger(commission))
+  );
 };
 
 module.exports = { feesCalculating };
